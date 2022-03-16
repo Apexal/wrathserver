5c4cba32b9a196c0deb7f99f4b3689e0908ea213
BianoBonzo <brianlin16@hotmail.com>
2022-03-16 14:35:12 -0400

    Fixed implementation for mp3 audio files

--- audio/normalizeAudio.py
+++ audio/normalizeAudio.py
@@ -4,14 +4,15 @@ from pydub import AudioSegment
 # for file formats.
 # --> pip install pydub
 # --> sudo apt-get install ffmpeg
-# NOTE: You have to use .wav audio formats 
 
 def match_target_amplitude(sound, target_dBFS):
     change_in_dBFS = target_dBFS - sound.dBFS
     return sound.apply_gain(change_in_dBFS)
 
 #sound = AudioSegment.from_file("SFXLoud.wav", "wav")
-sound = AudioSegment.from_file("SFXQuiet.wav", "wav")
+#sound = AudioSegment.from_file("SFXQuiet.wav", "wav")
+sound = AudioSegment.from_mp3("SFXLoud2.mp3")
 normalized_sound = match_target_amplitude(sound, -20.0)
+normalized_sound.export("SFXNormalizedLoud2.mp3", format="mp3")
 #normalized_sound.export("SFXNormalizedLoud.wav", format="wav")
-normalized_sound.export("SFXNormalizedQuiet.wav", format="wav")
+#normalized_sound.export("SFXNormalizedQuiet.wav", format="wav")
