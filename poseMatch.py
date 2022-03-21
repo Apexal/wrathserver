115c5836fdc7bfaafa9535ab59dd5e5d73b7462c
schnur <ryanschnurrpi@gmail.com>
2022-03-21 18:29:20 -0400

    Checks if pose is within given range

--- poseMatch.py
+++ poseMatch.py
@@ -3,7 +3,7 @@ from imageio import save
 import numpy as np
 from turtle import pos
 from PIL import Image, ImageDraw, ImageFilter
-
+import os
 
 def turnWhite(imageName, newName):
     img = Image.open(imageName+'.png')
@@ -21,13 +21,13 @@ def turnWhite(imageName, newName):
     img.save(newName+".png", "PNG") 
 
 def countBlackPixel(imageName):
-    img= Image.open(imageName+".png")
+    img= Image.open(imageName)
     img=img.convert("RGBA")
     datas=img.getdata()
 
     blackCount=0
     for item in datas:
-        if(item[0]==0 and item[1]==0 and item[2]==0):
+        if(item[0]==0 and item[1]==0 and item[2]==0 and item[3]>0):
             blackCount+=1
     img.close()   
     return blackCount   
@@ -36,25 +36,50 @@ def countBlackPixel(imageName):
 '''
 Given 2 400x400 Pixel pictures the move will be masked onto the move
 '''
-person="images\output-resized.png"
-pose="poses\light_punch\light_punch_pose_frame_0.png"
+def personPoseValid(person, pose):
+    #person="images\output-resized.png"
+    #pose="poses\light_punch\light_punch_pose_frame_0.png"
 
-imPerson=Image.open(person)
-imPose=Image.open(pose)
+    imPerson=Image.open(person)
+    imPose=Image.open(pose)
 
-imPerson2=imPerson.copy()
-temp=imPose.copy()
-temp.paste(imPerson,(0,0),imPerson)
-temp.save("fit.png", quality=95)
+    #imPerson2=imPerson.copy()
+    temp=imPose.copy()
+    temp.paste(imPerson,(0,0),imPerson)
+    temp.save("fit.png", quality=95)
 
-image = cv2.imread(person)
-print("person: "+str(np.sum(image == 0)))
-print("total: "+str(np.sum(image)))
-image = cv2.imread(pose)
-print("###############")
-print("pose: "+str(np.sum(image == 0)))
-print("total: "+str(np.sum(image)))
+    #image = cv2.imread(person)
+    #print("person: "+str(np.sum(image == 0)))
+    #print("total: "+str(np.sum(image)))
 
+    #add process to move picture to improve accuracy/allow it to pass
+    print("person")
+    blackPixels_on_person=countBlackPixel("test.png")
+    print(blackPixels_on_person)
+    print("pose")
+    blackPixels_on_pose=countBlackPixel(pose)
+    print(blackPixels_on_pose)
+    print("fit")
+    blackPixels_on_fit=countBlackPixel("fit.png")
+    print(blackPixels_on_fit-blackPixels_on_person)
+    print((blackPixels_on_fit-blackPixels_on_person)/blackPixels_on_pose)
+    returnPixelValue=temp.convert("RGBA").getdata()
+    os.remove("fit.png")
+
+
+    if((blackPixels_on_fit-blackPixels_on_person)/blackPixels_on_pose<.35):
+        #print("yes")
+        #print(returnPixelValue[0])
+        return (blackPixels_on_fit-blackPixels_on_person)/blackPixels_on_pose, returnPixelValue
+    else:
+        print("no")
+        return -1, []
+
+personPoseValid("images\output-resized.png","poses\light_punch\light_punch_pose_frame_0.png")
+#image = cv2.imread(pose)
+#print("pose: "+str(np.sum(image == 0)))
+#print("total: "+str(np.sum(image)))
+'''
 imgaPose=imPose.convert("RGBA")
 datas=imgaPose.getdata()
 
@@ -68,14 +93,16 @@ for item in datas:
         transparentCount+=1
     else:
         nontransparentCount+=1
-print("transparent "+str(transparentCount))
-print("nontransparent "+str(nontransparentCount))
-print("real total "+str(nontransparentCount+transparentCount))
-print("Black pixels "+str(blackCount))
-print("###############")
-image = cv2.imread("fit.png")
-print("fit: "+str(np.sum(image == 0)))
-print("total: "+str(np.sum(image)))
+'''
+#print("transparent "+str(transparentCount))
+#print("nontransparent "+str(nontransparentCount))
+#print("real total "+str(nontransparentCount+transparentCount))
+#print("Black pixels "+str(blackCount))
+#print("###############")
+
+#image = cv2.imread("fit.png")
+#print("fit: "+str(np.sum(image == 0)))
+#print("total: "+str(np.sum(image)))
 '''
 pose
 fit
@@ -91,31 +118,33 @@ imPerson2=imPerson2.crop((5,5,395,395))
 imPerson2=imPerson2.resize((400,400))
 imPerson2=imPerson2.crop((5,5,395,395))
 imPerson2=imPerson2.resize((400,400))'''
-imPerson2.save("test.png",quality=95)
-turnWhite("test","testW")
 
-whiteChar=Image.open("testW.png")
 
-current=Image.open("green.png")
-greenControl=current.copy()
+#imPerson2.save("test.png",quality=95)
+#turnWhite("test","testW")
 
-current.paste(whiteChar,(0,0),imPose)
-current.save("fit2.png", quality=95)
-current.save("fit3.png", quality=95)
+#whiteChar=Image.open("testW.png")
+
+#current=Image.open("green.png")
+#greenControl=current.copy()
+
+#current.paste(whiteChar,(0,0),imPose)
+#current.save("fit2.png", quality=95)
+#current.save("fit3.png", quality=95)
 
 """
 current=greenControl
 current.save("fit2.png", quality=95)
 """
 
-print(countBlackPixel("fit2"))
-direction=0
-lastCount=countBlackPixel("fit2")
+#print(countBlackPixel("fit2"))
+#direction=0
+#lastCount=countBlackPixel("fit2")
 
 #check if or image is completely in shadow
 #(Left,Top,Right,Bottom)
-stuck=0
-while(countBlackPixel("fit3")>0):
+#stuck=0
+'''while(countBlackPixel("fit3")>0):
     print(countBlackPixel("fit3"))
     imgKeep=current.copy()
     personKeep=imPerson2.copy()
@@ -169,7 +198,7 @@ while(countBlackPixel("fit3")>0):
     
 current=greenControl
 current.paste(imPerson2,(0,0),imPose)
-current.save("fitNew.png",quality=95)
+current.save("fitNew.png",quality=95)'''
 
 
 
