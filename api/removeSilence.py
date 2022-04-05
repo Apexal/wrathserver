52df97ff9e6b932a08a3a6e0169c2ccd17eaeb91
Tevetron <friedrich.jonathan4@gmail.com>
2022-04-05 16:50:18 -0400

    Testing Code for removing silences

---
+++ api/removeSilence.py
@@ -0,0 +1,19 @@
+import base64
+from io import BytesIO, StringIO
+from pydub import AudioSegment
+from pydub.silence import detect_leading_silence
+
+def trim_mp3_bytes(mp3_bytes: bytes) -> bytes:
+    input = AudioSegment.from_mp3(BytesIO(mp3_bytes))
+    trim_leading_silence: AudioSegment = lambda x: x[detect_leading_silence(x) :]
+    trim_trailing_silence: AudioSegment = lambda x: trim_leading_silence(x.reverse()).reverse()
+    strip_silence: AudioSegment = lambda x: trim_trailing_silence(trim_leading_silence(x))
+    buf = BytesIO()
+    trimmed_sound = strip_silence(input)
+    trimmed_sound.export(buf, format="mp3")
+    return buf.getvalue()
+
+def trim_mp3_b64(mp3_b64: str) -> str:
+    return base64.standard_b64encode(
+        trim_mp3_bytes(base64.standard_b64decode(mp3_b64))
+    ).decode("utf-8")
