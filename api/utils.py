8d29b9072212974d221c737a767150fe738ec263
Frank Matranga <thefrankmatranga@gmail.com>
2022-04-10 16:57:36 -0400

    Add calculate pose util

--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,8 @@
 import base64
+import math
 from PIL import Image
+
+from api.models import PoseLandmark
 
 
 def crop_to_content(img: Image.Image) -> Image.Image:
@@ -38,3 +41,18 @@ def expand_img_to_square(img: Image.Image):
         result = Image.new(img.mode, (height, height), background_color)
         result.paste(img, ((height - width) // 2, 0))
         return result
+
+
+def calculate_angle(a: PoseLandmark, b: PoseLandmark, c: PoseLandmark) -> float:
+    angle = math.degrees(
+        math.atan2(c.y - b.y, c.x - b.x) - math.atan2(a.y - b.y, a.x - b.x)
+    )
+
+    # Check if the angle is less than zero.
+    if angle < 0:
+
+        # Add 360 to the found angle.
+        angle += 360
+
+    # Return the calculated angle.
+    return angle
