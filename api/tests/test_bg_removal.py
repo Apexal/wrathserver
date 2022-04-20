c1d8393b3605af7417982c74f6681cb8efb3180f
Frank Matranga <thefrankmatranga@gmail.com>
2022-04-19 20:37:35 -0400

    Implement pose utility testing

--- api/tests/test_bg_removal.py
+++ api/tests/test_bg_removal.py
@@ -1,7 +1,3 @@
-from api.removeBackground import remove_background_b64
-from api.utils import b64_to_bytes, b64_to_image
-
-
 def test_bg_removal():
     # img = b64_to_bytes(b64_png)
     # removed_bg = remove_background(img)
