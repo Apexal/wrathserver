9b9a15464b2f3ce2feb98e2dec7a159aceb555a4
Frank Matranga <thefrankmatranga@gmail.com>
2022-04-28 16:23:20 -0400

    Add pydub converting methods

--- api/utils/converting.py
+++ api/utils/converting.py
@@ -1,6 +1,7 @@
 import base64
 from io import BytesIO
 from PIL import Image, ImageOps
+from pydub import AudioSegment
 
 
 def b64_to_bytes(b64: str) -> bytes:
@@ -21,3 +22,21 @@ def image_to_b64(img: Image.Image) -> str:
     buffered = BytesIO()
     img.save(buffered, format="PNG")
     return bytes_to_b64(buffered.getvalue())
+
+
+def b64_to_audio(b64: str, mimetype: str) -> AudioSegment:
+    audio_bytes = b64_to_bytes(b64)
+    if mimetype == "audio/mpeg":
+        input = AudioSegment.from_mp3(BytesIO(audio_bytes))
+    elif mimetype == "audio/ogg":
+        input = AudioSegment.from_file(BytesIO(audio_bytes), codec="opus")
+    else:
+        raise Exception("Unknown mimetype")
+
+    return input
+
+
+def audio_to_mp3_b64(audio: AudioSegment) -> str:
+    buf = BytesIO()
+    audio.export(buf, format="mp3")
+    return bytes_to_b64(buf.getvalue())
